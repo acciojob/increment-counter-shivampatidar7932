e4d459204d59140fef989a9bc487f9c1b2253e15
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-16 17:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ var counterElement = document.getElementById('counter');
+    var incrementBtn = document.getElementById('incrementBtn');
+var counterValue = 0;
+ function handleIncrement() {
+	  alert(counterValue);
+	  counterValue++;
+	  counterElement.textContent = counterValue;
+    }
+incrementBtn.addEventListener('click', handleIncrement);
